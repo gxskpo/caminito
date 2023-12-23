261475b1f9db6ff1206d4c9880885163f0e83985
haru <ggalactiko@galactiko.net>
2023-12-22 23:53:27 -0600

    Fixed image load i think

--- components/menu/menu-object.js
+++ components/menu/menu-object.js
@@ -1,18 +1,25 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import PlatilloSkeleton from "@/components/menu/obj-skeleton";
 import Image from "next/image";
 
-export default function MenuObject({ name, price = null, imageSrc, id , style}) {
+export default function MenuObject({name, price = null, imageSrc, id, style}) {
     const [loading, setLoading] = useState(true);
-
+    const [image, setImage] = useState(null);
     const handleImageLoad = () => {
         setLoading(false);
     };
+
+    useEffect(() => {
+        if (imageSrc) {
+            setImage(<img alt="Foto del platillo" src={imageSrc} onLoad={handleImageLoad} className='real-image'/>);
+        }
+    }, [imageSrc]);
+
     return (
         <div className="menu-object-cl" id={id} style={style}>
             <div className="image-container">
-                {<PlatilloSkeleton /> ? loading : null}
-                <img alt="Foto del platillo" src={imageSrc} onLoad={handleImageLoad} className="real-image"/>
+                {<PlatilloSkeleton/> ? loading : null}
+                {image}
             </div>
             <h3>{name}</h3>
             {<p>{price}</p> ? price : null}
