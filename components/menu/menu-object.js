c50c1425a195c9d04fdee9da5f2a439e8d82046a
haru <ggalactiko@galactiko.net>
2023-12-22 23:42:54 -0600

    Added skeleton for menu object images, better colors for menu objects, fixed zoom when chat-input is focused (mobile), added animation when menu detail is opened

--- components/menu/menu-object.js
+++ components/menu/menu-object.js
@@ -1,9 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import PlatilloSkeleton from "@/components/menu/obj-skeleton";
+import Image from "next/image";
 
 export default function MenuObject({ name, price = null, imageSrc, id , style}) {
+    const [loading, setLoading] = useState(true);
+
+    const handleImageLoad = () => {
+        setLoading(false);
+    };
     return (
         <div className="menu-object-cl" id={id} style={style}>
-            <img alt="Foto del platillo" src={imageSrc} />
+            <div className="image-container">
+                {<PlatilloSkeleton /> ? loading : null}
+                <img alt="Foto del platillo" src={imageSrc} onLoad={handleImageLoad} className="real-image"/>
+            </div>
             <h3>{name}</h3>
             {<p>{price}</p> ? price : null}
         </div>
---
+++ components/menu/obj-skeleton.js
@@ -0,0 +1,11 @@
+// PlatilloSkeleton.js
+import React from 'react';
+import Image from 'next/image';
+
+const PlatilloSkeleton = () => {
+    return (
+        <div className="skeleton-element"></div>
+    );
+};
+
+export default PlatilloSkeleton;
